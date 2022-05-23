97e02640f65247add2fbbbfab3ab3915f697456a
Helltraitor <helltraitor@hotmail.com>
2022-05-23 22:54:16 +0300

    Added warning on double module adding
    It's can be important in case when some module is reloading in the wrong way

--- media_manager/application/modules/keeper.py
+++ media_manager/application/modules/keeper.py
@@ -9,6 +9,7 @@ class ModulesKeeper:
 
     def module_add(self, module: Module):
         if module.id in self.__modules:
+            _double_module_add_warning(type(self).__name__, self.__modules[module.id], module)
             return
         self.__modules[module.id] = module
 
@@ -18,3 +19,21 @@ class ModulesKeeper:
 
     def module_all(self) -> list[Module]:
         return list(self.__modules.values())
+
+
+# Separated warning function to reduce cognitive load
+def _double_module_add_warning(cls_name: str, old: Module, new: Module):
+    old_module_meta = {
+        "meta": old.module_meta,
+        "name": old.module_meta.name() if old.module_meta is not None else "Error",
+        "version": old.module_meta.version() if old.module_meta is not None else "Error",
+    }
+    new_module_meta = {
+        "meta": new.module_meta,
+        "name": new.module_meta.name() if new.module_meta is not None else "Error",
+        "version": new.module_meta.version() if new.module_meta is not None else "Error",
+    }
+    logging.warning(f"{cls_name}: Attempting to override already loaded module with id `{old.id}`", extra={
+        "new_module_meta": new_module_meta,
+        "old_module_meta": old_module_meta,
+    })
