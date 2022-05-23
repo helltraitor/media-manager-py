190d040442a68f89b6aa1c51e060a76f8e1f286a
Helltraitor <helltraitor@hotmail.com>
2022-05-23 22:48:47 +0300

    Added logging (config)

--- __main__.py
+++ __main__.py
@@ -1,9 +1,15 @@
+import logging
+
 from pathlib import Path
 
 from media_manager.application import Application
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)7s: %(filename)s %(funcName)s: %(message)s",
+        level=logging.DEBUG)
+
     app_location = Path(__file__).parent
     modules_location = app_location / "media_manager" / "modules"
 
