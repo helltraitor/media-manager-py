d8607fce44d38f4a2f254ea805ecaff28e849338
Helltraitor <helltraitor@hotmail.com>
2022-05-23 20:30:39 +0300

    Added constants file

---
+++ media_manager/application/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+APPLICATION_ICON = str(Path(__file__).parent.parent / "resources" / "app-icon.svg")
+APPLICATION_NAME = "Media manager"
+APPLICATION_VERSION = "0.0.1"
+APPLICATION_MODULE_API_VERSION = "0.0.1"
