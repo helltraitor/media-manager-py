9b4c1516c6b9a888ad7f5fae4259794138a1e8f5
Helltraitor <helltraitor@hotmail.com>
2022-05-24 22:43:20 +0300

    Updated Callback class

--- media_manager/application/callbacks.py
+++ media_manager/application/callbacks.py
@@ -1,15 +1,30 @@
-from typing import Any, Callable
+from typing import Any, Callable, TypeAlias
 
 from media_manager.application.filters import Filter, NoFilter
 
 
+CallbackFunction: TypeAlias = Callable[[], Any]
+
+
 class Callback:
-    def __init__(self, callback: Callable[[], None], filter: Filter = NoFilter):
-        self.__callback = callback
-        self.__filter = filter
+    def __init__(self, *callbacks: CallbackFunction | "Callback"):
+        self.__callbacks = callbacks
+        self.__filter = NoFilter
 
     def call_on(self, obj: Any) -> bool:
-        if self.__filter.passed(obj):
-            self.__callback()
-            return True
-        return False
+        if not self.__filter.passed(obj):
+            return False
+
+        for callback in self.__callbacks:
+            if isinstance(callback, Callback):
+                callback.call_on(obj)
+            else:
+                callback()
+        return True
+
+    def with_filter(self, filter: Filter) -> "Callback":
+        if self.__filter is not NoFilter:
+            raise ValueError(
+                f'{type(self).__name__}: `with_filter` method can be called only once, when NoFilter is using')
+        self.__filter = filter
+        return self
