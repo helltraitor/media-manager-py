44cef1403235462f28dbd0d3a6fd6b80866883b6
Helltraitor <helltraitor@hotmail.com>
2022-05-26 00:53:52 +0300

    Modules loader updated

--- media_manager/application/modules/loader.py
+++ media_manager/application/modules/loader.py
@@ -1,14 +1,32 @@
 import logging
 
-from importlib import import_module
 from pathlib import Path
+from types import ModuleType
 from typing import Type
 
-from media_manager.application.api import ModuleLoader
+from media_manager.application.api.loader import ModuleLoader
+from media_manager.application.api.module import Module
 from media_manager.application.constants import APPLICATION_MODULE_API_VERSION
 
 from .keeper import ModulesKeeper
-from .module import Module
+
+
+class Import:
+    from importlib import import_module as unwrap
+
+    def __init__(self, name: str, package: str | None = None):
+        self.__module = None
+        self.__name = name
+        self.__package = package
+
+    def make(self) -> ModuleType | None:
+        if self.__module is None:
+            # noinspection PyBroadException
+            try:
+                self.__module = Import.unwrap(self.__name, self.__package)
+            except Exception:
+                pass
+        return self.__module
 
 
 class ImportLocations:
@@ -35,7 +53,7 @@ class ModulesLoader:
         self.app_location = app_location
         self.modules_locations: list[Path] = []
         self.modules_loaders: list[Type[ModuleLoader]] = []
-        self.modules_objects: dict[str, list[Module]] = {
+        self.modules_objects: dict[str, list[ModuleLoader]] = {
             "SUCCESS": [],
             "FAILURE": []
         }
@@ -56,7 +74,8 @@ class ModulesLoader:
                 # Find all modules names in indicated import locations
                 modules_names = (path.parts[-1] for path in location.iterdir() if path.is_dir())
                 # Import these modules
-                modules_objects = (module for module in map(import_module, modules_names))
+                modules_objects = (module.make() for module in map(Import, modules_names))
+                modules_objects = (module for module in modules_objects if module is not None)
                 # Get public api class or default None singleton
                 modules_loaders = (getattr(module, "PublicModuleLoader", object) for module in modules_objects)
                 # Filter loaders by subclass check (all loaders must be subclass of ModuleLoader)
@@ -64,23 +83,19 @@ class ModulesLoader:
                 self.modules_loaders.extend(modules_loaders)
 
     def load_all(self, keeper: ModulesKeeper):
-        modules = [Module(loader()) for loader in self.modules_loaders]
-        for module in sorted(modules, key=lambda mod: mod.loading_priority or 1):
-            if module.module_meta is None:
-                logging.error(f'{type(self).__name__}: Unable to load module without meta information')
-                self.modules_objects["FAILURE"].append(module)
-            elif not module.module_meta.is_supported_api(APPLICATION_MODULE_API_VERSION):
+        loaders = [loader() for loader in self.modules_loaders]
+        for loader in sorted(loaders, key=lambda lod: lod.loading_priority() or 1):
+            if not loader.is_api_supported(APPLICATION_MODULE_API_VERSION):
                 logging.error(' '.join((
                     f'{type(self).__name__}:',
-                    f'Unable to load module that is not support {APPLICATION_MODULE_API_VERSION} api version.',
-                    f'Module id is `{module.id}`')))
-                self.modules_objects["FAILURE"].append(module)
+                    f'Unable to load module that is not support {APPLICATION_MODULE_API_VERSION} api version.')))
+                self.modules_objects["FAILURE"].append(loader)
             else:
-                self.modules_objects["SUCCESS"].append(module)
-                keeper.module_add(module)
+                self.modules_objects["SUCCESS"].append(loader)
+                keeper.module_add(loader.load())
 
-    def fetch_failure(self) -> list[Module]:
+    def fetch_failure(self) -> list[ModuleLoader]:
         return self.modules_objects["FAILURE"]
 
-    def fetch_success(self) -> list[Module]:
+    def fetch_success(self) -> list[ModuleLoader]:
         return self.modules_objects["SUCCESS"]
