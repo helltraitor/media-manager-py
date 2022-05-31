8826b68148a5a828595e5b281cd1a7cc04303675
Helltraitor <helltraitor@hotmail.com>
2022-05-31 18:34:19 +0300

    Sidebar file was splitted

--- media_manager/application/widgets/sidebar/sidebar.py
+++ media_manager/application/widgets/sidebar/sidebar.py
@@ -13,71 +13,9 @@ from media_manager.application.api.events.module import (
 from media_manager.application.api.module import ModuleWidget
 
 from .listeners import SideBarWidgetFocusListener
+from .other import OtherWidgets
+from .system import SystemWidgets
 from .widget import SideBarWidget
-
-
-from PySide2 import QtWidgets
-from PySide2 import QtCore
-
-
-class OtherWidgets(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.__widgets: list[SideBarWidget] = []
-        self.__layout = QVBoxLayout(self)
-        # Setup
-        self.__setup()
-
-    def __setup(self):
-        # Layout
-        self.__layout.setContentsMargins(0, 0, 0, 0)
-        # Self
-        self.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Fixed))
-
-    def widgets(self) -> Iterator[SideBarWidget]:
-        return iter(self.__widgets)
-
-    def widget_add(self, module: ModuleWidget):
-        if self.widget_exists(module):
-            logging.warning(f'{type(self).__name__}: Attempting to add already added module widget: {type(module)}')
-            return
-
-        widget = SideBarWidget(module)
-        self.__widgets.append(widget)
-        self.__layout.addWidget(widget, alignment=Qt.AlignBottom)
-
-    def widget_exists(self, module: ModuleWidget) -> bool:
-        return module in map(SideBarWidget.module, self.__widgets)
-
-
-class SystemWidgets(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.__widgets: list[SideBarWidget] = []
-        self.__layout = QVBoxLayout(self)
-        # Setup
-        self.__setup()
-
-    def __setup(self):
-        # Layout
-        self.__layout.setContentsMargins(0, 0, 0, 0)
-        # Self
-        self.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.MinimumExpanding, QtWidgets.QSizePolicy.Policy.Fixed))
-
-    def widgets(self) -> Iterator[SideBarWidget]:
-        return iter(self.__widgets)
-
-    def widget_add(self, module: ModuleWidget):
-        if self.widget_exists(module):
-            logging.warning(f'{type(self).__name__}: Attempting to add already added module widget: {type(module)}')
-            return
-
-        widget = SideBarWidget(module)
-        self.__widgets.append(widget)
-        self.__layout.addWidget(widget, alignment=Qt.AlignBottom)
-
-    def widget_exists(self, module: ModuleWidget) -> bool:
-        return module in map(SideBarWidget.module, self.__widgets)
 
 
 class SideBar(QWidget):
---
+++ media_manager/application/widgets/sidebar/system.py
@@ -0,0 +1,40 @@
+import logging
+
+from typing import Iterator
+
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QVBoxLayout, QSizePolicy, QWidget
+
+from media_manager.application.api.module import ModuleWidget
+
+from .widget import SideBarWidget
+
+
+class SystemWidgets(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.__widgets: list[SideBarWidget] = []
+        self.__layout = QVBoxLayout(self)
+        # Setup
+        self.__setup()
+
+    def __setup(self):
+        # Layout
+        self.__layout.setContentsMargins(0, 0, 0, 0)
+        # Self
+        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+
+    def widgets(self) -> Iterator[SideBarWidget]:
+        return iter(self.__widgets)
+
+    def widget_add(self, module: ModuleWidget):
+        if self.widget_exists(module):
+            logging.warning(f'{type(self).__name__}: Attempting to add already added module widget: {type(module)}')
+            return
+
+        widget = SideBarWidget(module)
+        self.__widgets.append(widget)
+        self.__layout.addWidget(widget, alignment=Qt.AlignBottom)
+
+    def widget_exists(self, module: ModuleWidget) -> bool:
+        return module in map(SideBarWidget.module, self.__widgets)
