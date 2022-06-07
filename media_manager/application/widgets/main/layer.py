0628a96f06d1f33aa2833f43d146bf999b25dd85
Helltraitor <helltraitor@hotmail.com>
2022-06-07 23:05:06 +0300

    Added MainWidgetLayer class

--- media_manager/application/widgets/main/layer.py
+++ media_manager/application/widgets/main/layer.py
@@ -1,23 +1,16 @@
-import logging
-
-from PySide2.QtCore import QEvent
-from PySide2.QtWidgets import QWidget, QLayout
+from PySide2.QtWidgets import QWidget, QVBoxLayout
 
 from media_manager.application.api.module.window import ModuleWindow
 
 
 class MainWidgetLayer(QWidget):
     def __init__(self, window: ModuleWindow):
-        super().__init__(self)
-        self.__container = QLayout(self)
+        super().__init__()
+        self.__container = QVBoxLayout(self)
         self.__window = window
+        self.__setup()
 
     def __setup(self):
         # Layout
         self.__container.setContentsMargins(0, 0, 0, 0)
         self.__container.addWidget(self.__window.window())
-
-    def event(self, event: QEvent) -> bool:
-        for callback in self.__callbacks.values():
-            callback.call_on(event)
-        return False
