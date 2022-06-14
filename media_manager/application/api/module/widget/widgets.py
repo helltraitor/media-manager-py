db0f90b77d840d4e9d108d105c3a18de711220b6
Helltraitor <helltraitor@hotmail.com>
2022-06-15 00:11:05 +0300

    Updated default widget realizations

--- media_manager/application/api/module/widget/widgets.py
+++ media_manager/application/api/module/widget/widgets.py
@@ -9,23 +9,23 @@ from .abc import Widget, ModuleWidget
 
 
 class FocusableWidget(Widget):
-    def __init__(self, module: ModuleWidget):
-        super().__init__(module)
+    def __init__(self, widget: ModuleWidget):
+        super().__init__(widget)
 
     def mousePressEvent(self, event: QMouseEvent):
         super().mousePressEvent(event)
         if event.button() == Qt.LeftButton:
-            self.module.events.announce(WidgetFocusedEvent(self.module))
+            self.widget().events.announce(WidgetFocusedEvent(self.widget()))
 
 
 class HoverableWidget(Widget):
-    def __init__(self, module: ModuleWidget):
-        super().__init__(module)
+    def __init__(self, widget: ModuleWidget):
+        super().__init__(widget)
 
     def enterEvent(self, event: QEvent):
         super().enterEvent(event)
-        self.module.events.announce(WidgetHoveredEvent(self.module))
+        self.widget().events.announce(WidgetHoveredEvent(self.widget()))
 
     def leaveEvent(self, event: QEvent):
         super().leaveEvent(event)
-        self.module.events.announce(WidgetUnhoveredEvent(self.module))
+        self.widget().events.announce(WidgetUnhoveredEvent(self.widget()))
