f06bedfd241e326291537f87f7e0b15366221d0e
Helltraitor <helltraitor@hotmail.com>
2022-06-15 21:12:05 +0300

    Added value view in settings module

--- media_manager/modules/settings/window.py
+++ media_manager/modules/settings/window.py
@@ -1,16 +1,43 @@
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide2.QtWidgets import QLabel, QListWidget, QSizePolicy, QVBoxLayout, QWidget
+
+from .storage import Storage
+
+
+class TableWidget(QListWidget):
+    def __init__(self):
+        super().__init__()
+        self.__storage = Storage()
+        self.__setup()
+
+    def __setup(self):
+        self.__storage.subscribe("set", self.refill)
+        self.__storage.subscribe("delete", self.refill)
+        self.refill()
+
+    def refill(self):
+        self.clear()
+        self.addItems(tuple(f"`{key}` :: `{value}`" for key, value in self.__storage.all().items()))
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.__settings_title = QLabel()
+        self.__title = QLabel()
+        self.__table = TableWidget()
         self.__layout = QVBoxLayout(self)
         self.__setup()
 
     def __setup(self):
         self.__layout.setContentsMargins(6, 6, 6, 6)
-        # Labels
-        self.__settings_title.setText("Settings")
-        self.__layout.addWidget(self.__settings_title, alignment=Qt.AlignLeft)
+        # Label
+        font = self.__title.font()
+        font.setPointSize(18)
+        self.__title.setText("Settings")
+        self.__title.setFont(font)
+        self.__title.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum))
+        self.__layout.addWidget(self.__title, alignment=(Qt.AlignLeft | Qt.AlignTop))
+        # Table
+        self.__table.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
+        self.__layout.addSpacing(6)
+        self.__layout.addWidget(self.__table)
