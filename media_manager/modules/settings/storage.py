37e40cffb9a216c90cbf1ddf80ed03f802f2dbff
Helltraitor <helltraitor@hotmail.com>
2022-06-15 21:10:05 +0300

    Added basic storage class in settings module

---
+++ media_manager/modules/settings/storage.py
@@ -0,0 +1,36 @@
+from typing import Callable
+
+
+class Storage:
+    ONLINE_STORAGE: dict[str, str] = {}
+    CALLBACKS: dict[str, list[Callable[[], None]]] = {}
+
+    def __init__(self):
+        self.__online = self.ONLINE_STORAGE
+        self.__callbacks = self.CALLBACKS
+
+    def get(self, key: str, default: str | None = None) -> str | None:
+        self.notify("get")
+        return self.__online.get(key, default)
+
+    def set(self, key: str, value: str):
+        self.__online[key] = value
+        self.notify("set")
+
+    def delete(self, key: str, default: str | None = None) -> str | None:
+        value = self.__online.pop(key, default)
+        self.notify("delete")
+        return value
+
+    def all(self) -> dict[str, str]:
+        self.notify("all")
+        return self.__online.copy()
+
+    def notify(self, action: str):
+        for callback in self.__callbacks.get(action, ()):
+            callback()
+
+    def subscribe(self, action: str, callback: Callable[[], None]):
+        if action not in self.__callbacks:
+            self.__callbacks[action] = []
+        self.__callbacks[action].append(callback)
