8f8ed41b805e6d4c6c283976ebca22bc087b3cd1
Helltraitor <helltraitor@hotmail.com>
2022-06-20 21:33:48 +0300

    Added utils (name, runtime_cast)

---
+++ media_manager/application/utils.py
@@ -0,0 +1,21 @@
+import logging
+
+from typing import Any, Type, TypeVar
+
+T = TypeVar("T")
+
+
+def name(entity: Any) -> str:
+    if hasattr(entity, "__name__"):
+        return entity.__name__
+    if hasattr(entity, "__class__") and hasattr(entity.__class__, "__name__"):
+        return entity.__class__.__name__
+    logging.error("%s: Entity %s has no name", name.__name__, repr(entity))
+    raise RuntimeError(f"Entity {entity} has no name")
+
+
+def runtime_cast(inst: Any, guard: Type[T]) -> T:
+    if isinstance(inst, guard):
+        return inst
+    logging.error("%s: %s is not instance of %s", name(runtime_cast), name(inst), name(guard))
+    raise RuntimeError(f"{name(inst)} is not instance of {name(guard)}")
