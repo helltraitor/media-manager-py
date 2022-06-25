0a71ef2870580b767727d2f68d4e0cc76f898cdd
Helltraitor <helltraitor@hotmail.com>
2022-06-25 23:15:11 +0300

    Application client removed (probably temporary)

--- media_manager/application/application.py
+++ media_manager/application/application.py
@@ -7,24 +7,9 @@ from PySide2.QtWidgets import QApplication
 
 from media_manager.application.api.context import Context
 from media_manager.application.api.deferred import DeferredPool
-from media_manager.application.api.messages import MessageServer, MessageClient, Message, Reply
+from media_manager.application.api.messages import MessageServer
 from media_manager.application.widgets.window import SupportableModule, Window
 from media_manager.application.modules import Keeper, Loader
-
-
-class ApplicationClient(MessageClient):
-    def __init__(self, keeper: Keeper):
-        super().__init__("Application", {
-            "name": "Application"
-        })
-        self.__keeper = keeper
-
-    def accepts(self, credits: dict[str, str]) -> bool:
-        return self.__keeper.contains(id=credits.get("id", ""))
-
-    def receive(self, message: Message) -> Reply:
-        logging.info(message.content())
-        return Reply(message.content())
 
 
 class Application(QApplication):
@@ -54,10 +39,6 @@ class Application(QApplication):
             self.keeper.append(module)
             if isinstance(module, SupportableModule):
                 self.window.append(module)
-
-        # Anonymous application client
-        client = ApplicationClient(self.keeper)
-        client.connect(message_server)
 
     def start(self) -> int:
         self.__server_timer.start(0)
