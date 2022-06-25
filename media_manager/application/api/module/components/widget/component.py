8456a0e79b6df60a367fc6161b5d5fc43993cd49
Helltraitor <helltraitor@hotmail.com>
2022-06-25 23:14:03 +0300

    Default widget now can use context data

--- media_manager/application/api/module/components/widget/component.py
+++ media_manager/application/api/module/components/widget/component.py
@@ -14,27 +14,32 @@ class CWidget(Component):
     def __init__(self, context: Context):
         super().__init__(context)
         self.__events = EventPool()
+        self.__type = context.checked("type", str) or "Other"
 
     def events(self) -> EventPool:
         return self.__events
 
     def type(self) -> str:
-        return "Other"
+        return self.__type
 
     def widget(self) -> Widget:
         raise NotImplementedError("This method must be implemented in subclass")
 
 
 class CDefaultWidget(CWidget):
+    ICON = str(Path(__file__).parent.parent.parent.parent.parent.parent / "resources" / "carol-liao-solve-icon.svg")
+
     def __init__(self, context: Context):
         super().__init__(context)
+        self.__icon = context.checked("icon", str) or self.ICON
+        self.__title = context.checked("title", str) or "Unknown"
         self.__widget = DefaultWidget(ReferenceType(self))
 
     def icon(self) -> str:
-        return str(Path(__file__).parent.parent.parent.parent.parent.parent / "resources" / "carol-liao-solve-icon.svg")
+        return self.__icon
 
     def title(self) -> str:
-        raise NotImplementedError("This method must be implemented in subclass")
+        return self.__title
 
     def widget(self) -> DefaultWidget:
         return self.__widget
