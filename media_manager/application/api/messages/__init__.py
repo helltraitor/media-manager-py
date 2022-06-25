ddbc831d86cf18b681a8b68eac5943e3cc05e702
Helltraitor <helltraitor@hotmail.com>
2022-06-25 23:15:33 +0300

    Update message entry point

--- media_manager/application/api/messages/__init__.py
+++ media_manager/application/api/messages/__init__.py
@@ -1,4 +1,8 @@
 from .client import MessageClient
-from .message import Message, CallbackMessage
+from .credits import Credits
+from .handler import MessageHandler
+from .message import CallbackMessage, SignableMessage, SignedMessage
 from .reply import Reply
+from .result import Result
 from .server import MessageServer
+from .target import Target
