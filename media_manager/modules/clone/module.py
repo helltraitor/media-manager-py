1012d57bb18a9975d3db9b532807b33febbfe6e5
Helltraitor <helltraitor@hotmail.com>
2022-06-25 23:15:55 +0300

    Updated clone module

--- media_manager/modules/clone/module.py
+++ media_manager/modules/clone/module.py
@@ -1,29 +1,20 @@
 from random import randint
 
 from media_manager.application.api.context import Context
+from media_manager.application.api.messages import Credits
 from media_manager.application.api.module.loader import ModuleLoader
 from media_manager.application.api.module.factory import ModuleFactory, ModuleBuilder
 
-from media_manager.application.api.module.components import CMetaInformation
-from media_manager.application.api.module.features import FMetaInformation
+from media_manager.application.api.module.components import CMessageClient, CMetaInformation, CDefaultWidget
+from media_manager.application.api.module.features import FMessages, FMetaInformation, FDefaultWidget
 
-from .widget import CCloneDefaultWidget, FDefaultWidget
 from .window import CCloneWindow, FWindow
 
+RANDOM_ID = str(randint(0, 10**6))
 
-class CCloneMetaInformation(CMetaInformation):
-    def __init__(self, context: Context):
-        super().__init__(context)
-        self.__context = context
-
-    def id(self) -> str:
-        return f"{self.name()} ({self.version()})"
-
-    def name(self) -> str:
-        return f"Clone #{self.__context.unwrap('id', str)}"
-
-    def version(self) -> str:
-        return "0.0.1"
+NAME = f"Clone #{RANDOM_ID}"
+VERSION = "0.0.1"
+ID = f"{NAME} ({VERSION})"
 
 
 class PublicModuleLoader(ModuleLoader):
@@ -32,10 +23,15 @@ class PublicModuleLoader(ModuleLoader):
         return version.split(".", 3)[0] == "0"
 
     def load(self) -> ModuleBuilder:
-        clone_context = Context().with_object(str(randint(0, 10**6)), name="id")
-
+        custom = (Context()
+                  .with_object(ID, name="id")
+                  .with_object(NAME, name="title")
+                  .with_object(NAME, name="name")
+                  .with_object(VERSION, name="version")
+                  .with_object(Credits(NAME, VERSION, ID), name="credits"))
         return (ModuleFactory()
-                .install_component(FMetaInformation, CCloneMetaInformation, context=clone_context)
-                .install_component(FDefaultWidget, CCloneDefaultWidget, context=clone_context)
-                .install_component(FWindow, CCloneWindow, context=clone_context)
+                .install_component(FMetaInformation, CMetaInformation, context=custom)
+                .install_component(FMessages, CMessageClient, context=custom)
+                .install_component(FDefaultWidget, CDefaultWidget, context=custom)
+                .install_component(FWindow, CCloneWindow, context=custom)
                 .assemble())
--- media_manager/modules/clone/window.py
+++ media_manager/modules/clone/window.py
@@ -1,19 +1,22 @@
+import logging
 from weakref import ReferenceType
 
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import QPushButton, QLabel, QVBoxLayout
 
 from media_manager.application.api.context import Context
-from media_manager.application.api.messages import Message, CallbackMessage
-from media_manager.application.api.module.components import CWindow
+from media_manager.application.api.messages import CallbackMessage, SignableMessage, Target
+from media_manager.application.api.module import Module
+from media_manager.application.api.module.components import CMessageClient, CWindow
 from media_manager.application.api.module.components.window.abc import Window
-from media_manager.application.api.module.features import FWindow
+from media_manager.application.api.module.features import FMessages, FWindow
 
 
 class CloneWindow(Window):
     def __init__(self, component: ReferenceType[CWindow]):
         super().__init__(component)
-        self.__clone_title = QLabel(f'Clone #{self.component().id()}')
+        self.__client: CMessageClient | None = None
+        self.__clone_title = QLabel()
         self.__clone_up = QPushButton("^^^")
         self.__clone_value = QLabel("0")
         self.__clone_down = QPushButton("vvv")
@@ -21,53 +24,58 @@ class CloneWindow(Window):
         self.__setup()
 
     def __setup(self):
+        # GUI
+        self.__clone_down.setAutoRepeat(True)
+        self.__clone_up.setAutoRepeat(True)
+        #
         self.__layout.setContentsMargins(6, 6, 6, 6)
         self.__layout.addWidget(self.__clone_title, alignment=Qt.AlignTop)
         self.__layout.addSpacing(6)
         self.__layout.addWidget(self.__clone_up, alignment=Qt.AlignTop)
         self.__layout.addWidget(self.__clone_value, alignment=Qt.AlignTop)
         self.__layout.addWidget(self.__clone_down, alignment=Qt.AlignTop)
-        # INIT SETTING
-        # TODO: RECOVER CLIENT
-        # client = self.__window.module().client()
-        # client.send({"name": "Settings"}, Message({"action": "set", "key": f"{self.__id}-value", "value": "0"}))
-        # BUTTON
-        self.__clone_up.setAutoRepeat(True)
-        # self.__clone_up.clicked.connect(
-        #     lambda: change_value(client, self.__id, self.__clone_value, 1))
-        self.__clone_down.setAutoRepeat(True)
-        # self.__clone_down.clicked.connect(
-        #     lambda: change_value(client, self.__id, self.__clone_value, -1))
+
+    def link_stage_setup(self, name:str, id: str, client: CMessageClient):
+        self.__clone_title.setText(f"{name} [{id}]")
+
+        self.__client = client
+        self.__client.client().send(
+            Target("Settings"),
+            SignableMessage({"action": "set", "key": "counter", "value": "0"}))
+
+        self.__clone_up.clicked.connect(lambda: self.change_value(1))
+        self.__clone_down.clicked.connect(lambda: self.change_value(-1))
+
+    def change_value(self, delta: int):
+        if self.__client is None:
+            return
+
+        client = self.__client.client()
+        client.send(Target("Settings"), CallbackMessage(  # GET VALUE FROM STORAGE
+            {"action": "get", "key": "counter"},
+            lambda reply: client.send(Target("Settings"), CallbackMessage(  # CHANGE GOT VALUE
+                {"action": "set", "key": "counter", "value": str(int(reply.content().get("value", "0")) + delta)},
+                lambda _: client.send(Target("Settings"), CallbackMessage(  # PUT IT IN THE LABEL
+                    {"action": "get", "key": "counter"},
+                    lambda reply: self.__clone_value.setText(reply.content().get("value", "ERROR"))))))))
 
 
 class CCloneWindow(CWindow):
     def __init__(self, context: Context):
         super().__init__(context)
+        self.__id = context.unwrap("id", str)
+        self.__name = context.unwrap("name", str)
+
         self.__context = context
         self.__window = CloneWindow(ReferenceType(self))
 
     def id(self) -> str:
         return self.__context.unwrap('id', str)
 
+    def link(self, module: Module):
+        super().link(module)
+        client = module.components().get_unwrap(FMessages, CMessageClient)
+        self.__window.link_stage_setup(self.__name, self.__id, client)
+
     def window(self) -> Window:
         return self.__window
-
-
-def change_value(client, id, label, delta):
-    def update(value):
-        # then change fresh value
-        client.send({"name": "Settings"}, CallbackMessage(
-            # and update label
-            lambda reply: fetch_value(client, id, label),
-            {"action": "update", "key": f"{id}-value", "value": str(value)}))
-    # get fresh value
-    client.send({"name": "Settings"}, CallbackMessage(
-        # update after getting value
-        lambda reply: update(int(reply.content().get("value")) + delta), {
-            "action": "get", "key": f"{id}-value"}))
-
-
-def fetch_value(client, id, label):
-    client.send({"name": "Settings"}, CallbackMessage(
-                lambda reply: label.setText(reply.content().get("value", label.text())), {
-                    "action": "get", "key": f"{id}-value"}))
