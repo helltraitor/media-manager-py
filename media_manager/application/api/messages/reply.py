0c6940295a2571bd8e7c01f712ef5a719be5de7a
Helltraitor <helltraitor@hotmail.com>
2022-06-25 22:41:45 +0300

    Reply now contains result

--- media_manager/application/api/messages/reply.py
+++ media_manager/application/api/messages/reply.py
@@ -1,6 +1,18 @@
-class Reply:
-    def __init__(self, content: dict[str, str]):
-        self.__content = content
+from typing import TypeAlias
 
-    def content(self) -> dict[str, str]:
+from .result import Result
+
+
+Content: TypeAlias = dict[str, str]
+
+
+class Reply:
+    def __init__(self, result: Result, *, content: Content | None = None):
+        self.__content = content or {}
+        self.__result = result
+
+    def content(self) -> Content:
         return self.__content
+
+    def result(self) -> Result:
+        return self.__result
