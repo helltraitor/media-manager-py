707a59cffa246be85232c45b24ec1919815b92c0
Helltraitor <helltraitor@hotmail.com>
2022-06-25 23:14:31 +0300

    Metainformation can use data from context

--- media_manager/application/api/module/components/meta.py
+++ media_manager/application/api/module/components/meta.py
@@ -1,12 +1,29 @@
+import logging
+
+from media_manager.application import utils
+from media_manager.application.api.context import Context
+
 from .abc import Component
 
 
 class CMetaInformation(Component):
+    def __init__(self, context: Context):
+        super().__init__(context)
+        self.__id = context.checked("id", str)
+        self.__name = context.checked("name", str) or "Unknown"
+        self.__version = context.checked("version", str)
+
     def id(self) -> str:
-        raise NotImplementedError("This method must be implemented in subclass")
+        if self.__id is None:
+            logging.error("%s: Attempting to return None id instead of string", utils.name(self))
+            raise RuntimeError("Id must be provided in context or this method must be overrided")
+        return self.__id
 
     def name(self) -> str:
-        raise NotImplementedError("This method must be implemented in subclass")
+        return self.__name
 
     def version(self) -> str:
-        raise NotImplementedError("This method must be implemented in subclass")
+        if self.__version is None:
+            logging.error("%s: Attempting to return None version instead of string", utils.name(self))
+            raise RuntimeError("Version must be provided in context or this method must be overrided")
+        return self.__version
