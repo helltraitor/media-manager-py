a846272f2f8a1941361bb5970ee3e1f9b67ee1be
Helltraitor <helltraitor@hotmail.com>
2022-06-26 20:37:32 +0300

    Now icon shows in the windows taskbar

--- __main__.py
+++ __main__.py
@@ -1,9 +1,17 @@
+import ctypes
 import logging
+import os
+import pathlib
 import sys
 
-from pathlib import Path
-
 from media_manager.application import Application
+from media_manager.application.constants import (
+    APPLICATION_AUTHOR, APPLICATION_NAME, APPLICATION_VERSION, APPLICATION_API_VERSION
+)
+
+if os.name == "nt":
+    application_id = f"{APPLICATION_AUTHOR}:{APPLICATION_NAME}:{APPLICATION_VERSION}:{APPLICATION_API_VERSION}"
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(application_id)
 
 
 if __name__ == "__main__":
@@ -11,6 +19,6 @@ if __name__ == "__main__":
         format="%(asctime)s %(levelname)7s: %(filename)s %(funcName)s: %(message)s",
         level=logging.DEBUG)
 
-    location = Path(__file__).parent
+    location = pathlib.Path(__file__).parent
     application = Application(location)
     sys.exit(application.start())
